83eea295e68beb1d87ba5f920f831d6036dcdd98
saratrickett <strickett4@gmail.com>
2018-01-27 21:19:49 -0500

    standard library practice

--- head_first_python/standard_library.py
+++ head_first_python/standard_library.py
@@ -13,3 +13,8 @@ print(datetime.date.today())
 print(datetime.date.today().day)
 print(datetime.date.today().month)
 print(datetime.date.today().year)
+print(datetime.date.isoformat(datetime.date.today()))
+
+import time
+print(time.strftime('%H:%M:%S'))
+print(time.strftime ('%A %p'))
