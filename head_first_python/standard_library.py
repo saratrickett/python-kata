feb225ace349eb499e0a4ca26391ca737153ab09
saratrickett <strickett4@gmail.com>
2018-01-27 21:11:20 -0500

    standard library practice

---
+++ head_first_python/standard_library.py
@@ -0,0 +1,15 @@
+import sys
+print(sys.version)
+print(sys.platform)
+
+# name of folder you are operating in
+import os
+print(os.getcwd())
+print (os.environ)
+# system's environment variables
+
+import datetime
+print(datetime.date.today())
+print(datetime.date.today().day)
+print(datetime.date.today().month)
+print(datetime.date.today().year)
