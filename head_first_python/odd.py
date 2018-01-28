1b8bd1023b78792f7ea8e36ebef8368f32206a12
saratrickett <strickett4@gmail.com>
2018-01-27 22:15:30 -0500

    odd number practice

--- head_first_python/odd.py
+++ head_first_python/odd.py
@@ -2,12 +2,15 @@ from datetime import datetime
 """first line imports preexisting functionality from Python's standard library (stock of softwared modules providing prebuilt
 and reusable code"""
 
-odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
 
-right_this_minute = datetime.today().minute
+for i in range(5):
+    right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("This minute seems a little odd.")
-else:
-    print("Not an odd minute.")
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute.")
 
