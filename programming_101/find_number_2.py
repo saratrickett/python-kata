a331f2644b6802ba1c911905be7a20f0bfd7dace
saratrickett <strickett4@gmail.com>
2018-02-01 12:22:43 -0500

    Add more Python datatype katas

---
+++ programming_101/find_number_2.py
@@ -0,0 +1,17 @@
+# DATA TYPES
+#typing.io - typing practice for programmers
+# str
+_str = 'test'
+_str2 = "tst"
+_str3 = '{_str2} test'.format(_str2=_str2)
+_str4 = f'{_str} test'
+
+# bool
+_bool = True
+_bool_false = False
+
+# int
+_int = 1
+_int_negative = -1
+
+
