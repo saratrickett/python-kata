02313486584d093b0ee6f32c23b73ca021e2f84f
saratrickett <strickett4@gmail.com>
2018-02-03 22:08:19 -0500

    find number practice

---
+++ programming_101/find_number3.py
@@ -0,0 +1,59 @@
+# DATA TYPES
+# str - string
+# bool - boolean
+# int - integer
+# float
+# list
+
+# CASTING
+# str + int = runtime exception
+
+
+# CONTROL STRUCTURES
+# --for loops--
+# for {variable_name} in <collection>:
+#     <action>
+#
+# --logical--
+# if <bool>:
+#   pass
+# elif <bool>:
+#   pass
+# else <bool>:
+#   pass
+#
+# --comparisons--
+# == -> equals
+# != -> not equals
+# > -> greater than
+# >= -> greater than equal
+# < -> less than
+# <= -> les than equal
+
+
+"""
+  PRACTICE: print each letter in a given string
+"""
+
+string = "pineapple"
+
+for letter in string:
+    print(letter)
+"""
+  PRACTICE: create a function that takes an input,
+  then prints each character of the input
+"""
+def print_character(input):
+    for letter in input:
+        print(letter)
+print_character('Sara')
+"""
+    PRACTICE: create a function that takes two inputs,
+    then prints True/False whether or not the first input
+    is contained within the second input
+"""
+def contain(first, second):
+    for letter in second:
+        if letter == first:
+            print(True)
+contain('r', 'Sara')
