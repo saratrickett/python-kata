64038b6c11d5092100c2102ce96db3c20f4c3627
saratrickett <strickett4@gmail.com>
2018-02-11 21:24:28 -0500

    more practice

--- head_first_python/mymodules/vsearch.py
+++ head_first_python/mymodules/vsearch.py
@@ -1,12 +1,10 @@
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
     """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     found = vowels.intersection(set(phrase))
     return found
 
-def search4letters (phrase: str, letters:str = 'aeiou') -> set:
+
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
-
-
-
