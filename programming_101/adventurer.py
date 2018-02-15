93a007d75c9a99e8977c8979d8a3f0b45b0878ef
saratrickett <strickett4@gmail.com>
2018-02-15 13:06:26 -0500

    Add to adventurer

---
+++ programming_101/adventurer.py
@@ -0,0 +1,39 @@
+import random
+
+class Adventurer:
+    def __init__(self, map, name, strength):
+        self.map = map
+        self.name = name
+        self.strength = strength
+
+
+    def receive_map(self, other_adventurer):
+        other_map = other_adventurer.map
+        self.map = other_map
+
+def generate_character(map):
+    strength = random.randit(1, 18)
+    adventurer = Adventurer(map, name, strength)
+    return adventurer
+
+good_map = 'good map'
+bad_map = 'bad map'
+
+justin = generate_character(bad_map, 'justin')
+alex = generate_character(good_map, 'alex')
+sara = generate_character(bad_map, 'sara')
+grande = generate_character(good_map, 'grande')
+
+adventurers = [justin, alex, sara, grande]
+
+for adventurers in party:
+    print(adventurers.map + ':' + str(adventurer.strength))
+print(justin.map)
+print(alex.map)
+print(sara.map)
+print(grande.map)
+
+sara.receive_map(grande)
+sara.map = good_map
+
+print('sara' + sara.map)
